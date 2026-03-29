7ce889dcf6522f58e75b084c7662675b8416d487
Bhoomika <Bhoo@gmail.com>
2026-03-29 14:04:24 +0530

    f

---
+++ app.py
@@ -0,0 +1,23 @@
+import gradio as gr
+from env import CloudEnv
+from models import Action
+from grader import grade
+
+def run_simulation():
+    env = CloudEnv()
+    obs = env.reset()
+    done = False
+
+    while not done:
+        if obs.pending_tasks > 0:
+            action = Action(action_type="create", cpu=2, memory=4)
+        else:
+            action = Action(action_type="delete", vm_id="0")
+
+        obs, reward, done, _ = env.step(action)
+
+    score = grade(env)
+    return f"Final Score: {score}"
+
+iface = gr.Interface(fn=run_simulation, inputs=[], outputs="text")
+iface.launch(server_name="0.0.0.0", server_port=7860)
